1d746758fe4418816252ddf0c5ce07344bbaf129
Jim Arnold <jim@thoughtworks.com>
2015-09-22 15:00:35 -0400

    moved each service pipeline into its own group; split catalog unit and functional tests into pipelines; functional pipeline pulls build artifact from unit test pipeline

--- playbooks/roles/configure_gocd/files/configure_gocd.py
+++ playbooks/roles/configure_gocd/files/configure_gocd.py
@@ -7,23 +7,29 @@ CF_PASSWORD = os.environ['CF_PASSWORD']
 
 configurator = GoCdConfigurator(HostRestClient("localhost:8153"))
 
-pipeline = configurator\
-	.ensure_pipeline_group("pretend")\
-	.ensure_replacement_of_pipeline("pretend_catalog_service")\
+pipeline_group = configurator.ensure_pipeline_group("catalog")
+	
+pipeline = pipeline_group\
+  .ensure_replacement_of_pipeline("catalog_unit_tests")\
 	.set_git_url("https://github.com/ThoughtWorks-AELab/pretend_catalog_service")
-pipeline.ensure_unencrypted_secure_environment_variables({"CF_EMAIL": CF_EMAIL, "CF_PASSWORD": CF_PASSWORD})
 stage = pipeline.ensure_stage("Test")
 job = stage.ensure_job("UnitTest")
 job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle install --path vendor/bundle --without production']))
 job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle exec rake spec:unit']))
 job.ensure_artifacts({TestArtifact("spec/reports")})
+job.ensure_artifacts({BuildArtifact("*", "build")})
+
+pipeline = pipeline_group\
+    .ensure_replacement_of_pipeline("catalog_functional_tests")\
+    .ensure_material(PipelineMaterial('catalog_unit_tests', 'Test'))
+pipeline.ensure_unencrypted_secure_environment_variables({"CF_EMAIL": CF_EMAIL, "CF_PASSWORD": CF_PASSWORD})
 stage = pipeline.ensure_stage("DeployStaging")
 job = stage.ensure_job("Deploy")
-job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle install --path vendor/bundle --without production']))
-job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle exec rake deploy_dev']))
+job.add_task(FetchArtifactTask('catalog_unit_tests', 'Test', 'UnitTest', FetchArtifactDir('build')))
+job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle exec rake deploy_dev'], 'build'))
 
-pipeline = configurator\
-	.ensure_pipeline_group("pretend")\
+pipeline_group = configurator.ensure_pipeline_group("deals")
+pipeline = pipeline_group\
 	.ensure_replacement_of_pipeline("pretend_deals_service")\
 	.set_git_url("https://github.com/ThoughtWorks-AELab/pretend_deals_service")
 pipeline.ensure_unencrypted_secure_environment_variables({"CF_EMAIL": CF_EMAIL, "CF_PASSWORD": CF_PASSWORD})
@@ -32,30 +38,35 @@ job = stage.ensure_job("UnitTest")
 job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle install --path vendor/bundle --without production']))
 job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle exec rake spec:unit']))
 job.ensure_artifacts({TestArtifact("spec/reports")})
+
 stage = pipeline.ensure_stage("DeployStaging")
 job = stage.ensure_job("Deploy")
 job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle install --path vendor/bundle --without production']))
 job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle exec rake deploy_dev']))
 
-pipeline = configurator\
-	.ensure_pipeline_group("pretend")\
-	.ensure_replacement_of_pipeline("pretend_pricing_service")\
+pipeline_group = configurator.ensure_pipeline_group("pricing")
+pipeline = pipeline_group\
+	.ensure_replacement_of_pipeline("pricing_unit_tests")\
 	.set_git_url("https://github.com/ThoughtWorks-AELab/pretend_pricing_service")
-pipeline.ensure_unencrypted_secure_environment_variables({"CF_EMAIL": CF_EMAIL, "CF_PASSWORD": CF_PASSWORD})
 stage = pipeline.ensure_stage("UnitTest")
 job = stage.ensure_job("UnitTest")
 job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle install --path vendor/bundle']))
 job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle exec rake db:migrate']))
 job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle exec rake spec:unit']))
 job.ensure_artifacts({TestArtifact("spec/reports")})
+
+pipeline = pipeline_group\
+    .ensure_replacement_of_pipeline("pricing_functional_tests")\
+    .ensure_material(PipelineMaterial('pricing_unit_tests', 'UnitTest'))
+pipeline.ensure_unencrypted_secure_environment_variables({"CF_EMAIL": CF_EMAIL, "CF_PASSWORD": CF_PASSWORD})
 stage = pipeline.ensure_stage("FunctionalTest")
 job = stage.ensure_job("FunctionalTest")
 job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle install --path vendor/bundle']))
 job.add_task(ExecTask(['/bin/bash', '-l', '-c', 'bundle exec rake spec:functional']))
 job.ensure_artifacts({TestArtifact("spec/reports")})
 
-pipeline = configurator\
-	.ensure_pipeline_group("pretend")\
+pipeline_group = configurator.ensure_pipeline_group("web_app")
+pipeline = pipeline_group\
 	.ensure_replacement_of_pipeline("pretend_web_app")\
 	.set_git_url("https://github.com/ThoughtWorks-AELab/pretend_web_app")
 pipeline.ensure_unencrypted_secure_environment_variables({"CF_EMAIL": CF_EMAIL, "CF_PASSWORD": CF_PASSWORD})
