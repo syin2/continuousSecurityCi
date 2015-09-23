105c23a6f079ebc469791e24930f88964e2af785
Patrick Turley [pturley] <patrick.turley@gmail.com>
2015-09-23 14:37:18 -0500

    having deals functional test bring up an instance of pricing service that it connects to for testing

--- playbooks/roles/configure_gocd/files/configure_gocd.py
+++ playbooks/roles/configure_gocd/files/configure_gocd.py
@@ -2,20 +2,15 @@
 from gomatic import *
 import os
 
-configurator = GoCdConfigurator(HostRestClient("localhost:8153"))
-
 def _create_pipeline(group, pipeline_name, add_cf_vars=False):
 	pipeline_group = configurator.ensure_pipeline_group(group)
 	pipeline = pipeline_group.ensure_replacement_of_pipeline(pipeline_name)
 	if(add_cf_vars == True):
-		pipeline.ensure_unencrypted_secure_environment_variables({"CF_EMAIL": os.environ['CF_EMAIL'], "CF_PASSWORD": os.environ['CF_PASSWORD']})
+		pipeline.ensure_unencrypted_secure_environment_variables({"CF_EMAIL": os.environ['CF_EMAIL'], "CF_PASSWORD": os.environ['CF_PASSWORD'], "CF_HOME": "."})
 	return pipeline
 
-def _add_exec_task(job, command, working_dir=None):
-	if working_dir == None:
-		job.add_task(ExecTask(['/bin/bash', '-l', '-c', command]))
-	else:
-		job.add_task(ExecTask(['/bin/bash', '-l', '-c', command], working_dir))
+def _add_exec_task(job, command, working_dir=None, runif="passed"):
+	job.add_task(ExecTask(['/bin/bash', '-l', '-c', command], working_dir=working_dir, runif=runif))
 
 def build_catalog_pipeline_group(configurator):
 	pipeline = _create_pipeline("catalog", "catalog_unit_tests")
@@ -46,7 +41,7 @@ def build_pricing_pipeline_group(configurator):
 	pipeline.ensure_material(PipelineMaterial('pricing_unit_tests', 'test'))
 	job = pipeline.ensure_stage("test").ensure_job("test")
 	job.add_task(FetchArtifactTask('pricing_unit_tests', 'test', 'test', FetchArtifactDir('pricing_build')))
-	_add_exec_task(job, 'bundle exec rake spec:functional', 'pricing_build')
+	_add_exec_task(job, 'APP_NAME=pricing_$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER bundle exec rake spec:functional', 'pricing_build')
 	job.ensure_artifacts({TestArtifact("spec/reports")})
 
 def build_deals_pipeline_group(configurator):
@@ -64,7 +59,9 @@ def build_deals_pipeline_group(configurator):
 	job = pipeline.ensure_stage("test").ensure_job("test")
 	job.add_task(FetchArtifactTask('pricing_unit_tests', 'test', 'test', FetchArtifactDir('pricing_build')))
 	job.add_task(FetchArtifactTask('deals_unit_tests', 'test', 'test', FetchArtifactDir('deals_build')))
-	_add_exec_task(job, 'bundle exec rake spec:functional', 'deals_build')
+	_add_exec_task(job, 'bundle exec rake app:deploy[test,pricing_$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER,pricing_$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER]', 'pricing_build')
+	_add_exec_task(job, 'PRICING_SERVICE_URL=http://pricing_$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER.cfapps.io APP_NAME=deals_$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER bundle exec rake spec:functional', 'deals_build')
+	_add_exec_task(job, 'bundle exec rake app:delete[test,pricing_$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER]', 'pricing_build', "any")
 	job.ensure_artifacts({TestArtifact("spec/reports")})
 
 def build_web_app_pipeline_group(configurator):
@@ -79,6 +76,7 @@ def build_web_app_pipeline_group(configurator):
 	_add_exec_task(job, 'bundle install --path vendor/bundle --without production')
 	_add_exec_task(job, 'bundle exec rake deploy_dev')
 
+configurator = GoCdConfigurator(HostRestClient("localhost:8153"))
 build_catalog_pipeline_group(configurator)
 build_pricing_pipeline_group(configurator)
 build_deals_pipeline_group(configurator)
