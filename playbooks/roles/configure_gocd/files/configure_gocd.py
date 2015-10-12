8e487d5c4617978fef44bf978d5bd1b2662ed30e
Patrick Turley [pturley] <patrick.turley@gmail.com>
2015-10-12 16:42:33 -0500

    Adding production deployments

--- playbooks/roles/configure_gocd/files/configure_gocd.py
+++ playbooks/roles/configure_gocd/files/configure_gocd.py
@@ -29,6 +29,14 @@ def build_catalog_pipeline_group(configurator):
 	_add_exec_task(job, 'PREFIX=$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER bundle exec rake spec:functional', 'catalog_build')
 	job.ensure_artifacts(set([BuildArtifact("catalog_build/*", "catalog_build"), TestArtifact("catalog_build/spec/reports")]))
 
+	pipeline = _create_pipeline("catalog", "catalog_deploy_production", True)
+	pipeline.ensure_material(PipelineMaterial('catalog_functional_tests', 'test'))
+	stage = pipeline.ensure_stage("Deploy_Production")
+	stage.set_has_manual_approval()
+	job = stage.ensure_job("deploy")
+	job.add_task(FetchArtifactTask('catalog_functional_tests', 'test', 'test', FetchArtifactDir('catalog_build')))
+	_add_exec_task(job, 'bundle exec rake cf:deploy[production]', 'catalog_build')
+
 def build_pricing_pipeline_group(configurator):
 	pipeline = _create_pipeline("pricing", "pricing_unit_tests")
 	pipeline.set_git_url("https://github.com/ThoughtWorks-AELab/pretend_pricing_service")
@@ -45,6 +53,14 @@ def build_pricing_pipeline_group(configurator):
 	job.add_task(FetchArtifactTask('pricing_unit_tests', 'test', 'test', FetchArtifactDir('pricing_build')))
 	_add_exec_task(job, 'PREFIX=$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER bundle exec rake spec:functional', 'pricing_build')
 	job.ensure_artifacts(set([BuildArtifact("pricing_build/*", "pricing_build"), TestArtifact("pricing_build/spec/reports")]))
+
+	pipeline = _create_pipeline("pricing", "pricing_deploy_production", True)
+	pipeline.ensure_material(PipelineMaterial('pricing_functional_tests', 'test'))
+	stage = pipeline.ensure_stage("Deploy_Production")
+	stage.set_has_manual_approval()
+	job = stage.ensure_job("deploy")
+	job.add_task(FetchArtifactTask('pricing_functional_tests', 'test', 'test', FetchArtifactDir('pricing_build')))
+	_add_exec_task(job, 'bundle exec rake cf:deploy[production]', 'pricing_build')
 
 def build_deals_pipeline_group(configurator):
 	pipeline = _create_pipeline("deals", "deals_unit_tests")
@@ -67,6 +83,14 @@ def build_deals_pipeline_group(configurator):
 	_add_exec_task(job, 'parallel "cd {}; bundle exec rake cf:delete[test,$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER];" ::: pricing_build deals_build', runif='any')
 	_add_exec_task(job, 'parallel "cd {}; bundle exec rake cf:dups[test,$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER];" ::: pricing_build deals_build', runif='any')
 	job.ensure_artifacts(set([BuildArtifact("deals_build/*", "deals_build"), BuildArtifact("pricing_build/*", "pricing_build"), TestArtifact("deals_build/spec/reports")]))
+
+	pipeline = _create_pipeline("deals", "deals_deploy_production", True)
+	pipeline.ensure_material(PipelineMaterial('deals_functional_tests', 'test'))
+	stage = pipeline.ensure_stage("Deploy_Production")
+	stage.set_has_manual_approval()
+	job = stage.ensure_job("deploy")
+	job.add_task(FetchArtifactTask('deals_functional_tests', 'test', 'test', FetchArtifactDir('deals_build')))
+	_add_exec_task(job, 'bundle exec rake cf:deploy[production]', 'deals_build')
 
 def build_web_app_pipeline_group(configurator):
 	pipeline = _create_pipeline("web_app", "web_app_unit_tests", True)
@@ -91,7 +115,15 @@ def build_web_app_pipeline_group(configurator):
 	_add_exec_task(job, 'BASE_URL=http://$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER-web-app.cfapps.io bundle exec rake spec:functional', 'web_app_build')
 	_add_exec_task(job, 'parallel "cd {}; bundle exec rake cf:delete[test,$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER];" ::: catalog_build pricing_build deals_build web_app_build', runif='any')
 	_add_exec_task(job, 'parallel "cd {}; bundle exec rake cf:dups[test,$GO_PIPELINE_NAME$GO_PIPELINE_COUNTER];" ::: catalog_build pricing_build deals_build web_app_build', runif='any')
-	job.ensure_artifacts({TestArtifact("web_app_build/spec/reports")})
+	job.ensure_artifacts(set([BuildArtifact("web_app_build/*", "web_app_build"), TestArtifact("web_app_build/spec/reports")]))
+
+	pipeline = _create_pipeline("web_app", "web_app_deploy_production", True)
+	pipeline.ensure_material(PipelineMaterial('web_app_functional_tests', 'test'))
+	stage = pipeline.ensure_stage("Deploy_Production")
+	stage.set_has_manual_approval()
+	job = stage.ensure_job("deploy")
+	job.add_task(FetchArtifactTask('web_app_functional_tests', 'test', 'test', FetchArtifactDir('web_app_build')))
+	_add_exec_task(job, 'bundle exec rake cf:deploy[production]', 'web_app_build')
 
 configurator = GoCdConfigurator(HostRestClient("localhost:8153"))
 build_catalog_pipeline_group(configurator)
